d39766597848c79c51670b6f14f14064069017b2
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2025-04-18 20:04:10 -0300

    checklistItemService completed

--- src/main/java/com/learning/springboot/cheklistapi/entity/ChecklistItemEntity.java
+++ src/main/java/com/learning/springboot/cheklistapi/entity/ChecklistItemEntity.java
@@ -4,7 +4,9 @@ package com.learning.springboot.cheklistapi.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.time.LocalTime;
+
+import java.time.LocalDate;
+
 
 @Data
 @Entity
@@ -19,10 +21,58 @@ public class ChecklistItemEntity extends BaseEntity{
 
     private Boolean isCompleted;
 
-    private LocalTime deadline;
+    private LocalDate deadline;
 
-    private LocalTime postDate;
+    private LocalDate postDate;
 
     @ManyToOne
     private CategoryEntity category;
+
+    public Long getChecklistItemId() {
+        return checklistItemId;
+    }
+
+    public void setChecklistItemId(Long checklistItemId) {
+        this.checklistItemId = checklistItemId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(Boolean completed) {
+        isCompleted = completed;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public LocalDate getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(LocalDate postDate) {
+        this.postDate = postDate;
+    }
+
+    public CategoryEntity getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryEntity category) {
+        this.category = category;
+    }
 }
--- src/main/java/com/learning/springboot/cheklistapi/service/ChecklistItemService.java
+++ src/main/java/com/learning/springboot/cheklistapi/service/ChecklistItemService.java
@@ -1,9 +1,82 @@
 package com.learning.springboot.cheklistapi.service;
 
+import com.learning.springboot.cheklistapi.entity.CategoryEntity;
+import com.learning.springboot.cheklistapi.entity.ChecklistItemEntity;
+import com.learning.springboot.cheklistapi.exceptions.ResourceNotFoundException;
+import com.learning.springboot.cheklistapi.repository.CategoryRepository;
+import com.learning.springboot.cheklistapi.repository.ChecklistItemRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
 
 @Slf4j
 @Service
 public class ChecklistItemService {
+
+    private static final Logger log = LogManager.getLogger(ChecklistItemService.class);
+    @Autowired
+    private ChecklistItemRepository checklistItemRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    private void validateChecklistItemData(String description, Boolean isCompleted, LocalDate deadline, String categoryGuid){
+        if(!StringUtils.hasText(description)){
+            throw new IllegalArgumentException("Checklist item must have a description");
+        }
+
+        if(!StringUtils.hasText(categoryGuid)){
+            throw new IllegalArgumentException("Checklist item must have a description");
+        }
+
+        if(isCompleted == null){
+            throw new IllegalArgumentException("Checklist item must have a flag indicating if it is completed or not");
+        }
+
+        if(deadline == null){
+            throw new IllegalArgumentException("Checklist item must have a flag indicating if it is completed or not");
+        }
+    }
+
+    public ChecklistItemEntity addNewChecklistitem(String description, Boolean isCompleted, LocalDate deadline, String categoryGuid){
+
+        this.validateChecklistItemData(description, isCompleted, deadline,categoryGuid);
+
+       CategoryEntity retrievedCategory = this.categoryRepository.findByGuid(categoryGuid).orElseThrow(() -> new ResourceNotFoundException("Category not found"));
+
+        ChecklistItemEntity checklistItemEntity = new ChecklistItemEntity();
+        checklistItemEntity.setGuid(UUID.randomUUID().toString());
+        checklistItemEntity.setDescription(description);
+        checklistItemEntity.setDeadline(deadline);
+        checklistItemEntity.setPostDate(LocalDate.now());
+        checklistItemEntity.setCategory(retrievedCategory);
+
+        log.debug("Adding new checklist item [ checklistItem = {} ]", checklistItemEntity);
+
+        return checklistItemRepository.save(checklistItemEntity);
+    }
+
+    public Iterable<ChecklistItemEntity> findallChecklistItems(){
+        return this.checklistItemRepository.findAll();
+    }
+
+    public void deleteChecklistItem(String guid){
+        if(!StringUtils.hasText(guid)){
+            throw new IllegalArgumentException("Guid cannot be null or empty");
+        }
+        ChecklistItemEntity retrivedItem = this.checklistItemRepository.findByGuid(guid).orElseThrow(() -> new ResourceNotFoundException("Checklist item not found"));
+
+        log.debug("Deleting checklist item [ guid ={} ]", guid);
+
+        this.checklistItemRepository.delete(retrivedItem);
+    }
+
+
 }
