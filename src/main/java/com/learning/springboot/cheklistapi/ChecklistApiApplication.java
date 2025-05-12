f8c4f3bdcf7739475e14f9b8589312a29947955b
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2025-05-12 20:56:42 -0300

    ultimas alterações

--- src/main/java/com/learning/springboot/cheklistapi/ChecklistApiApplication.java
+++ src/main/java/com/learning/springboot/cheklistapi/ChecklistApiApplication.java
@@ -2,12 +2,27 @@ package com.learning.springboot.cheklistapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ChecklistApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChecklistApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfig(){
+		return new WebMvcConfigurer() {
+
+			@Override 
+			public void addCorsMappings(CorsRegistry corsRegistry){
+
+			}
+
+		};
 	}
 
 }
